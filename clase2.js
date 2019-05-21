e7b76e2fa1cb4cb96fdd27b506c98b57eeb0935d
mistersoftware <lcmojica@gmail.com>
2019-05-21 18:49:42 -0500

    concatenando texto

--- clase2.js
+++ clase2.js
@@ -3,4 +3,5 @@ var nombre = 'luisk' , apellido = 'mrsoftware'
 var nombreEnMayusculas = nombre.toUpperCase()
 var apellidoEnMinusculas = apellido.toLowerCase()
 var primeraLetraDelNombre = nombre.charAt(0)
-var cantidadDeLetrasDelNombre = nombre.lenght
+var cantidadDeLetrasDelNombre = nombre.length
+var nombreCompleto = nombre + ' ' + apellido
