c729b5082a4785bcd7830e8a89f274fd4ec55243
mistersoftware <lcmojica@gmail.com>
2019-05-21 18:52:00 -0500

    concatenando texto de otra forma: interpolacion de texto, se usa con comillas invertidas

--- clase2.js
+++ clase2.js
@@ -5,3 +5,4 @@ var apellidoEnMinusculas = apellido.toLowerCase()
 var primeraLetraDelNombre = nombre.charAt(0)
 var cantidadDeLetrasDelNombre = nombre.length
 var nombreCompleto = nombre + ' ' + apellido
+var nombreCompleto2 = `${nombre} ${apellido}`
