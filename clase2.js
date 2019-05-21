d639e7f7f59a54014cce05ccdb8ea59f9b59a0bd
mistersoftware <lcmojica@gmail.com>
2019-05-21 18:48:06 -0500

    diferentes funcioens y atributos para trabajar con strings

--- clase2.js
+++ clase2.js
@@ -2,4 +2,5 @@ var nombre = 'luisk' , apellido = 'mrsoftware'
 
 var nombreEnMayusculas = nombre.toUpperCase()
 var apellidoEnMinusculas = apellido.toLowerCase()
-
+var primeraLetraDelNombre = nombre.charAt(0)
+var cantidadDeLetrasDelNombre = nombre.lenght
