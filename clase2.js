bc327e57272927ab6e09d52945fe4e70822ee13e
mistersoftware <lcmojica@gmail.com>
2019-05-21 18:40:05 -0500

    trabajando con strings

--- clase2.js
+++ clase2.js
@@ -1,3 +1,5 @@
 var nombre = 'luisk' , apellido = 'mrsoftware'
 
-var nombreEnMayusculas = nombre.toUpperCase
+var nombreEnMayusculas = nombre.toUpperCase()
+var apellidoEnMinusculas = apellido.toLowerCase()
+
