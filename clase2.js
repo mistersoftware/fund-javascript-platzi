caf8be037a69155df331b20805a79bfb21ae1e40
mistersoftware <lcmojica@gmail.com>
2019-05-21 19:01:24 -0500

    acceder a un substring opcion 2 con substring el 1 indica el primer caracter y el 2 indica la cantidad de caracteres desde ahi

--- clase2.js
+++ clase2.js
@@ -7,3 +7,4 @@ var cantidadDeLetrasDelNombre = nombre.length
 var nombreCompleto = nombre + ' ' + apellido
 var nombreCompleto2 = `${nombre} ${apellido.toUpperCase()}`;
 var str = nombre.charAt(1) + nombre.charAt(2)
+var str2 = nombre.substr(1, 2)
