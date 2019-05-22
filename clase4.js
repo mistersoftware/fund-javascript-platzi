2c8c05b503b6e32549039766c8068d900e7a2c91
mistersoftware <lcmojica@gmail.com>
2019-05-21 19:31:42 -0500

    cambiamos el nombre de los argumentos dentro de la funcion para poder reutilizarla

--- clase4.js
+++ clase4.js
@@ -1,7 +1,7 @@
 var nombre = 'Luisk', edad = 38
 
-function imprimirEdad(){
-    console.log(`${nombre} tiene ${edad} años`)
+function imprimirEdad(n, e){
+    console.log(`${n} tiene ${e} años`)
 }
 
-imprimirEdad()
+imprimirEdad(nombre, edad)
