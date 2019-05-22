b69cb1961ab58cbe059bb3afbe9bb73e42ca263c
mistersoftware <lcmojica@gmail.com>
2019-05-21 19:15:19 -0500

    cuando usamos decimales ocurre algo particular, incluye un valor final asi 600.9000000000001 cuando esperabamos 600.9 es decir no es tan preciso

--- clase3.js
+++ clase3.js
@@ -2,9 +2,15 @@ var edad = 39
 var edad2 = 39
 var peso = 86
 var peso2 = 86
+var sandwich = 1
 
 edad = edad + 1
 edad2 += 1
 
 var peso = peso -2
 
+peso = peso + sandwich
+
+var precioDeVino = 200.3
+
+var total = precioDeVino * 3
